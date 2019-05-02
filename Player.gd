628b27361bab7682313c048ae06111837cba2105
narubl <bustamantenahuel10@gmail.com>
2019-05-02 15:36:18 -0300

    refinamiento 1

--- Player.gd
+++ Player.gd
@@ -1,17 +1,19 @@
 extends KinematicBody2D
 
-var p_velocity = Vector2(0,0)
+var p_velocity = Vector2(15,0) # Otorga movimiento inicial
 
 func _physics_process(delta):
-	getMove()
+	# Checkea si se presionaron teclas
+	get_move()
+	# Aplica las fisicas de movimiento
 	move_and_slide(p_velocity)
 
-func getMove():
+func get_move():
 	if Input.is_action_pressed("ui_right"):
-		p_velocity.x += 10
+		p_velocity.x += 15
 	if Input.is_action_pressed("ui_left"):
-		p_velocity.x -= 10
+		p_velocity.x -= 15
 	if Input.is_action_pressed("ui_up"):
-		p_velocity.y -= 10
+		p_velocity.y -= 15
 	if Input.is_action_pressed("ui_down"):
-		p_velocity.y += 10
+		p_velocity.y += 15
